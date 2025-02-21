ffadb5d90bbae59a78a8e750571c8ea78bd231a5
Michael Rogov Papernov <revolmich@gmail.com>
2025-02-21 15:57:10 +0000

    added norway, fixed index.html

--- filter_stations.py
+++ filter_stations.py
@@ -7,19 +7,24 @@ def read_compressed_json(file_path):
         data = json.load(f)
     return data
 
+empty_inventory = {'model': {'start': None, 'end': None}, 'hourly': {'start': None, 'end': None}, 'daily': {'start': None, 'end': None}, 'monthly': {'start': None, 'end': None}, 'normals': {'start': None, 'end': None}}
+
 def filter_points(points, countries):
+    print("Before filtering: ", len(points))
     points = [point for point in points if point.get('country') in countries]
-    points = [point for point in points if not all(value is None for value in point.get('inventory', {}).values())]
+    print("After filtering countries: ", len(points))
+    points = [point for point in points if point.get('inventory', empty_inventory) != empty_inventory]
+    print("After filtering inventory: ", len(points))
     return points
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process a compressed JSON file.')
+    parser = argparse.ArgumentParser(description='Filter weather stations by country and relevant data')
     parser.add_argument('file_path', type=str, help='The path to the compressed JSON file')
     args = parser.parse_args()
 
     file_path = args.file_path
     points = read_compressed_json(file_path)
-    selected_countries = ['NL', 'DE', 'DK', 'BE', 'FR', 'GB']
+    selected_countries = ['NL', 'DE', 'DK', 'BE', 'FR', 'GB', 'NO'] # Countries around the North Sea
     filtered_points = filter_points(points, selected_countries)
     output_file_path = file_path.replace('.json.gz', '_filtered.json.gz')
     with gzip.open(output_file_path, 'wt', encoding='utf-8') as f:
